a88df756b2eb5ddf94ade23821b42651899d0027
Alghraybi <rayanalgh@gmail.com>
2019-03-08 19:24:05 +0300

    stupid commit

--- objects/advertisement.php
+++ objects/advertisement.php
@@ -1,8 +1,8 @@
 <?php
 class Advertisment{
     //connections to the database
-    private $connection;
-    private $tableName;
+    private $conn;
+    private $tableName = 'advertisement';
 
     //Table attirbutes
     public $AD_id;
@@ -13,6 +13,15 @@ class Advertisment{
 
     public function __construct($db){
         $this->connection = $db;
+    }
+
+    public function read(){
+        $query = 'SELECT * FROM ' .$this->tableName . ' ';
+        
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->execute();
+        return $stmt;
     }
 }
 ?>
